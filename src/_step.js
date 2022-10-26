b821e6f5a9a0339f163b1ff0d2551cf1edf7fc49
nazmuldip <nazmuldip21@gmail.com>
2022-10-27 00:55:46 +0600

    workflow set in my step.js file

---
+++ src/_step.js
@@ -0,0 +1,46 @@
+/*
+====================
+install items
+1. react router
+2. react icons
+3. tailwind css
+4. daisyUI
+5.firebase
+====================
+*/
+
+
+
+/*
+====================
+Firebase 
+1. Add firebase.config.js file
+2. export app
+3.
+4.
+5.
+====================
+*/
+
+/*
+====================
+Add as Main Pages
+1. Home
+2. Courses
+3. Blog
+4. About
+5. Contact
+6. Apply
+
+
+====================
+*/
+/*
+====================
+Share Page
+1. Top Header
+2. Header
+3.Right Side Bar
+
+====================
+*/
